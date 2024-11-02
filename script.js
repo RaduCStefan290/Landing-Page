d939e08f43dcb985369bf96f9c8e969daf46b11d
Radu Stefan <raduconstantin23@stud.ase.ro>
2024-11-02 22:44:30 +0200

    landing-page

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const hamburger = document.querySelector('.hamburger');
-    const mobileNav = document.querySelector('.mobile-nav');
-
-    hamburger.addEventListener('click', function() {
-        mobileNav.classList.toggle('active');
-    });
-});
